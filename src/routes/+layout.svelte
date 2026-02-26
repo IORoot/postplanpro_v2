4e01fa107c0f3400e5f4f59611cdb9a577bc3c08
Andy Pearson <andy@londonparkour.com>
2026-02-26 18:27:05 +0000

    Update favicon link in layout to use direct path instead of imported asset, streamlining asset management.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import { onMount } from 'svelte';
 import { page } from '$app/stores';
 	import { initTheme } from '$lib/stores/theme.js';
@@ -17,7 +16,7 @@ import { page } from '$app/stores';
 </script>
 
 <svelte:head>
-	<link rel="icon" href={favicon} />
+	<link rel="icon" href="/favicon.svg" type="image/svg+xml" />
 	<title>PostPlan – Webhook Post Planner</title>
 </svelte:head>
 
